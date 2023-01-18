178a3b2e5968b31afd5cf81e3ddd7647cbe8c687
karmit <karmittech2004@gmail.com>
2023-01-18 13:01:59 +0530

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import './App.css';
-import LargeWithAppLinksAndSocial from './components/Home/Footer/footer';
 import Home from './pages/Home';
-import AdminPanel from './pages/Admin';
 function App() {
   return (
     <div className="App">
-      {/* <Home/>
-      <LargeWithAppLinksAndSocial/> */}
-<AdminPanel/>
+      <Home/>
     </div>
   );
 }
