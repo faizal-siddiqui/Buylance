5f2a03ab8a2c04ad00799f791d81bbdce9fd7d86
karmit <karmittech2004@gmail.com>
2023-01-19 20:36:01 +0530

    made some changes

--- src/pages/Karmit/Home.tsx
+++ src/pages/Karmit/Home.tsx
@@ -14,12 +14,7 @@ function Home() {
     <div>
       <Navbar/>
       <div
-        style={{
-          width: "90%",
-          height: "480px",
-          margin: "auto",
-          marginTop: "30px",
-        }}
+        className="carousel"
       >
         <Carousel images={images} interval={5000} />
       </div>
@@ -104,44 +99,17 @@ function Home() {
           />
         </div>
       </div>
-      <div
-        className="banner-1"
-        style={{
-          height: "550px",
-          width: "90%",
-          margin: "auto",
-          marginTop: "20px",
-          backgroundColor: "#ebf9ff",
-        }}
-      >
+      <div className="banner-1">
         <div>
-          <img
-            style={{ height: "100%" }}
-            src="https://imagescdn.abof.com/img/app/shopmedia/production/1/13-62-10521.jpg?w=501.27000000000004&auto=format"
-            alt="img_alt"
-          />
+          <img style={{height:"100%",width:"100%"}} src="https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
         </div>
         <div>
-          <img
-            style={{ height: "100%" }}
-            src="https://imagescdn.abof.com/img/app/shopmedia/production/1/13-62-10522.jpg?w=1002.5400000000001&auto=format"
-            alt="img_alt"
-          />
+          <img style={{height:"100%",width:"100%"}} src="https://images.unsplash.com/photo-1594969155368-f19485a9d88c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZmFzaGlvbiUyMGJ1eSUyMG5vd3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
         </div>
       </div>
       <div
-        className="banner-2"
-       
-      >
-        <div className="subbanner-1">
-          <h1
-           
-          >
-            BEST BUY STORE
-          </h1>
-          <button>VIEW ALL</button>
-        </div>
-        <div className="subbanner-2">
+        className="banner-2">
+      
           <img
             src="https://imagescdn.abof.com/img/app/shopmedia/production/1/13-62-9972.jpg?w=190&auto=format"
             alt="img_alt"
@@ -158,7 +126,7 @@ function Home() {
             src="https://imagescdn.abof.com/img/app/shopmedia/production/1/13-62-9978.jpg?w=190&auto=format"
             alt="img_alt"
           />
-        </div>
+        
       </div>
       <Footer/>
     </div>
