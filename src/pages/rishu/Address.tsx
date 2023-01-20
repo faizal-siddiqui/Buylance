4f9b61da5702ce98fcee416ea31a0b08f075412a
rishuraj99 <singhrj137@gmail.com>
2023-01-20 23:52:13 +0530

    d3

--- src/pages/rishu/Address.tsx
+++ src/pages/rishu/Address.tsx
@@ -1,13 +1,344 @@
-import React from 'react'
+import Navbar from "../../components/Navbar/navbar";
+import LargeWithAppLinksAndSocial from "../../components/Footer/footer/footer";
+import React, { useState } from "react";
+import {
+  Progress,
+  Box,
+  ButtonGroup,
+  Button,
+  Heading,
+  Flex,
+  FormControl,
+  GridItem,
+  FormLabel,
+  Input,
+  Select,
+  SimpleGrid,
+  InputLeftAddon,
+  InputGroup,
+  Textarea,
+  FormHelperText,
+  InputRightElement,
+} from "@chakra-ui/react";
 
-type Props = {}
+import { useToast } from "@chakra-ui/react";
+import { type } from "@testing-library/user-event/dist/type";
 
-const Address = (props: Props) => {
+const Form2 = () => {
   return (
-    <div>
-      Address
-    </div>
-  )
+    <>
+      <Heading w="100%" textAlign={"center"} fontWeight="normal" mb="2%">
+        User Details
+      </Heading>
+
+      <FormControl as={GridItem} colSpan={6}>
+        <FormLabel
+          htmlFor="street_address"
+          fontSize="sm"
+          fontWeight="md"
+          color="gray.700"
+          _dark={{
+            color: "gray.50",
+          }}
+          mt="2%"
+        >
+          Street address
+        </FormLabel>
+        <Input
+          type="text"
+          name="street_address"
+          id="street_address"
+          autoComplete="street-address"
+          focusBorderColor="brand.400"
+          shadow="sm"
+          size="sm"
+          w="full"
+          rounded="md"
+        />
+      </FormControl>
+
+      <Flex alignItems={"center"} gap="15px" mt={"15px"}>
+        <FormControl as={GridItem} colSpan={[6, 6, null, 2]}>
+          <FormLabel
+            htmlFor="city"
+            fontSize="sm"
+            fontWeight="md"
+            color="gray.700"
+            _dark={{
+              color: "gray.50",
+            }}
+            mt="2%"
+          >
+            City
+          </FormLabel>
+          <Input
+            type="text"
+            name="city"
+            id="city"
+            autoComplete="city"
+            focusBorderColor="brand.400"
+            shadow="sm"
+            size="sm"
+            w="full"
+            rounded="md"
+          />
+        </FormControl>
+
+        <FormControl as={GridItem} colSpan={[6, 3, null, 2]}>
+          <FormLabel
+            htmlFor="state"
+            fontSize="sm"
+            fontWeight="md"
+            color="gray.700"
+            _dark={{
+              color: "gray.50",
+            }}
+            mt="2%"
+          >
+            State / Province
+          </FormLabel>
+          <Input
+            type="text"
+            name="state"
+            id="state"
+            autoComplete="state"
+            focusBorderColor="brand.400"
+            shadow="sm"
+            size="sm"
+            w="full"
+            rounded="md"
+          />
+        </FormControl>
+      </Flex>
+
+      <Flex alignItems={"center"} gap="15px" mt={"15px"}>
+        <FormControl>
+          <FormLabel
+            htmlFor="postal_code"
+            fontSize="sm"
+            fontWeight="md"
+            color="gray.700"
+            _dark={{
+              color: "gray.50",
+            }}
+          >
+            ZIP / Postal
+          </FormLabel>
+          <Input
+            type="text"
+            name="postal_code"
+            id="postal_code"
+            autoComplete="postal-code"
+            focusBorderColor="brand.400"
+            shadow="sm"
+            size="sm"
+            w="full"
+            rounded="md"
+          />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel
+            htmlFor="country"
+            fontSize="sm"
+            fontWeight="md"
+            color="gray.700"
+            _dark={{
+              color: "gray.50",
+            }}
+          >
+            Country / Region
+          </FormLabel>
+          <Select
+            id="country"
+            name="country"
+            autoComplete="country"
+            placeholder="Select option"
+            focusBorderColor="brand.400"
+            shadow="sm"
+            size="sm"
+            w="full"
+            rounded="md"
+          >
+            <option>United States</option>
+            <option>Canada</option>
+            <option>Mexico</option>
+          </Select>
+        </FormControl>
+      </Flex>
+    </>
+  );
+};
+
+const Form3 = () => {
+  return (
+    <>
+      <Heading w="100%" textAlign={"center"} fontWeight="normal">
+        Social Handles
+      </Heading>
+      <SimpleGrid columns={1} spacing={6}>
+        <FormControl as={GridItem} colSpan={[3, 2]}>
+          <FormLabel
+            fontSize="sm"
+            fontWeight="md"
+            color="gray.700"
+            _dark={{
+              color: "gray.50",
+            }}
+          >
+            Website
+          </FormLabel>
+          <InputGroup size="sm">
+            <InputLeftAddon
+              bg="gray.50"
+              _dark={{
+                bg: "gray.800",
+              }}
+              color="gray.500"
+              rounded="md"
+            >
+              http://
+            </InputLeftAddon>
+            <Input
+              type="tel"
+              placeholder="www.example.com"
+              focusBorderColor="brand.400"
+              rounded="md"
+            />
+          </InputGroup>
+        </FormControl>
+
+        <FormControl id="email" mt={1}>
+          <FormLabel
+            fontSize="sm"
+            fontWeight="md"
+            color="gray.700"
+            _dark={{
+              color: "gray.50",
+            }}
+          >
+            About
+          </FormLabel>
+          <Textarea
+            placeholder="you@example.com"
+            rows={3}
+            shadow="sm"
+            focusBorderColor="brand.400"
+            fontSize={{
+              sm: "sm",
+            }}
+          />
+          <FormHelperText>
+            Brief description for your profile. URLs are hyperlinked.
+          </FormHelperText>
+        </FormControl>
+      </SimpleGrid>
+    </>
+  );
+};
+
+type Props = {
+
+};
+
+interface AddressType {
+  street: string;
+  city: string;
+  state: string;
+  pincode: string;
+  country: string;
 }
 
-export default Address
+const Address = (props: Props) => {
+  const [address, setAddress] = useState<AddressType>({
+    street: "",
+    city: "",
+    state: "",
+    pincode: "",
+    country: "",
+  });
+
+
+  const toast = useToast();
+  const [step, setStep] = useState<number>(1);
+  const [progress, setProgress] = useState<number>(50);
+  
+  const addAddress = () =>{
+    
+  }
+  return (
+    <>
+      <Navbar />
+
+      <Box
+        borderWidth="1px"
+        rounded="lg"
+        shadow="sm"
+        maxWidth={800}
+        p={6}
+        m="50px auto"
+        as="form"
+      >
+        <Progress
+          hasStripe
+          value={progress}
+          mb="5%"
+          mx="5%"
+          isAnimated
+        ></Progress>
+        {step === 1 ? <Form2 /> : <Form3 />}
+        <ButtonGroup mt="5%" w="100%">
+          <Flex w="100%" justifyContent="space-between">
+            <Flex>
+              <Button
+                onClick={() => {
+                  setStep(step - 1);
+                  setProgress(progress - 50);
+                }}
+                isDisabled={step === 1}
+                colorScheme="teal"
+                variant="solid"
+                w="7rem"
+                mr="5%"
+              >
+                Back
+              </Button>
+              <Button
+                w="7rem"
+                isDisabled={step === 2}
+                onClick={() => {
+                  setStep(step + 1);
+                  if (step === 2) {
+                    setProgress(100);
+                  } else {
+                    setProgress(progress + 50);
+                  }
+                }}
+                colorScheme="teal"
+                variant="outline"
+              >
+                Next
+              </Button>
+            </Flex>
+            {step === 2 ? (
+              <Button
+                w="7rem"
+                colorScheme="red"
+                variant="solid"
+                onClick={() => {
+                  
+                }}
+              >
+                Submit
+              </Button>
+            ) : null}
+          </Flex>
+        </ButtonGroup>
+      </Box>
+
+      <LargeWithAppLinksAndSocial />
+    </>
+  );
+};
+
+export default Address;
