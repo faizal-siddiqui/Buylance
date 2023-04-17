8c23f878ed3b0c2cea160a7f318a63411a59a3cd
faizal-siddiqui <faizalsidp@gmail.com>
2023-04-17 22:57:29 +0530

    updated responsiveness

--- src/components/Navbar/UpdatedNavbar.tsx
+++ src/components/Navbar/UpdatedNavbar.tsx
@@ -1,20 +1,33 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import styles from "./UpdatedNavbar.module.css";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 type Props = {};
 
 const UpdatedNavbar = (props: Props) => {
   const [username, setusername] = useState("Login");
+  const [openHam, setOpenHam] = useState(false);
+
+  const toggleHam = () => {
+    setOpenHam(!openHam);
+  };
 
   return (
     <header className={styles.header}>
       <div className={styles.navbar}>
+        <div className={styles.ham} onClick={toggleHam}>
+          <RxHamburgerMenu />
+        </div>
         <div>
           <img className={styles.logo} src="./logo.png" alt="logo" />
         </div>
-        <ul className={styles.nav}>
+        <ul
+          className={
+            openHam ? `${styles.nav} ${styles.openHamMenu}` : styles.nav
+          }
+        >
           <li>
             <Link to="/mens">Men</Link>
             <div className={styles.megamenu}></div>
